9342f266c0ca5b8ac507d91f78e9bfed25e5ad1b
Craig Oliver <coliver988@gmail.com>
2025-09-05 12:01:23 -0400

    WIP: switch to ranges instead of zones [BROKEN]

--- TravSystem/Data/Models/TStellarZones.cs
+++ TravSystem/Data/Models/TStellarZones.cs
@@ -4,8 +4,9 @@ public class TStellarZones
 {
     public int Id { get; set; }
     public int TStellarTypeId { get; set; }
-    public int Orbit { get; set; }
-    public string Zone { get; set; } = string.Empty; // e.g. "Inner", "Middle", "Outer"
+    public int InnerZoneLow { get; set; }
+    public int InnerZoneHigh { get; set; }
+    public int HabitableZone { get; set; }
     public string StarType { get; set; } = string.Empty; // e.g. "B", "A", "M", etc
 
     public TStellarTypes? TStellarType { get; set; } // Navigation property to TStellarTypes
