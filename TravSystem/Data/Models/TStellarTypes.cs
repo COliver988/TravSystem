f184546a5aae70bcdb7b8d1a1f90d86f4e0a8a41
Craig Oliver <coliver988@gmail.com>
2026-03-15 16:17:09 -0400

    added FullDisplayName for stellar sizes to get 1a Supergiantin listes more easily

--- TravSystem/Data/Models/TStellarTypes.cs
+++ TravSystem/Data/Models/TStellarTypes.cs
@@ -4,11 +4,13 @@ public class TStellarTypes
 {
     public int Id { get; set; }
 
-    // Supergirant, amin sequence, etc
+    // Supergirant, main sequence, etc
     public string Type { get; set; }
 
     // i1, 1b, II, etc
     public string Size { get; set; }
+    
+    public string FullDisplayName => $"{Size} {Type}";
 
     public List<TStellarZones> StellarZones { get; set; } = new List<TStellarZones>();
 }
